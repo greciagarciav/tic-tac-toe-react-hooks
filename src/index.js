672d04938f2aa1f21501b9f0d740afeb3823dac4
Grecia Villacorta <ggarciavillacort@tripadvisor.com>
2022-01-30 12:28:58 -0500

    update class components to use hooks

--- src/index.js
+++ src/index.js
@@ -1,68 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
+function Square(props) {
+    const[value] = useState(props);
+
+    return (
         <button className="square">
-            {this.props.value}
+            {value}
         </button>
-        );
-    }
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-        <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
-        </div>
-        );
-    }
+    );
 }
   
-class Game extends React.Component {
-    render() {
-        return (
+function Board(props) {
+    Square(props); 
+    <Square value={props} />;
+    const status = 'Next player: X';
+
+    return(
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {Square(0)}
+            {Square(1)}
+            {Square(2)}
+          </div>
+          <div className="board-row">
+            {Square(3)}
+            {Square(4)}
+            {Square(5)}
+          </div>
+          <div className="board-row">
+            {Square(6)}
+            {Square(7)}
+            {Square(8)}
+          </div>
+        </div>
+    );
+};
+
+function Game() {
+    return (
         <div className="game">
             <div className="game-board">
-            <Board />
+                <Board />
             </div>
             <div className="game-info">
             <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+                <ol>{/* TODO */}</ol>
             </div>
         </div>
-        );
-    }
+    );
 }
+
+// ========================================
   
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+ReactDOM.render(
+<Game />,
+document.getElementById('root')
+);
